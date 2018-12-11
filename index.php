23fcc65e3ccfcb67b7d2ed3e44c5537d8c2113e8
Sergio Garcia <sergio.garcia@interacso.com>
2018-12-11 21:04:40 +0100

    conect base de datos

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
 
 require './vendor/autoload.php';
 
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host'] = 'localhost';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['dbname'] = 'allyournew';
+
 $app = new \Slim\App;
 
 // Se crea un container que servira para añadir mas 
@@ -14,10 +22,29 @@ $container = $app->getContainer();
 // Añadios nueva depenencias para renderizar plantillas .phtml
 $container['view'] = new \Slim\Views\PhpRenderer('./src/views/');
 
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
 // Importamos el resto de codigo de nuestra aplicacion 
 // categorizado por funcionalidad.
 require './src/routes/index.php';
 require './src/api/index.php';
+// require __DIR__ . '/src/db/Classes/Models/News.php';
+// require __DIR__ . '/src/db/Classes/Models/User.php';
+// require __DIR__ . '/src/db/Classes/Controllers/AdminPanelController.php';
+// require __DIR__ . '/src/db/Classes/Controllers/NewsController.php';
+// require __DIR__ . '/src/db/Classes/Controllers/UserController.php';
+// require __DIR__ . '/src/db/Classes/Dao/UserDao.php';
+// require __DIR__ . '/src/db/Classes/Dao/NewsDao.php';
+
+// session_start();
+
 
 // Se lanza toda la aplicacion.
 $app->run();
