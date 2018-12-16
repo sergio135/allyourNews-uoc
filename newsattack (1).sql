87ec987fabbe6c20ee59374f3cd2d3de68f31ad0
paumg <38279644+paumg@users.noreply.github.com>
2018-12-16 16:15:38 +0100

    base de datos

---
+++ newsattack (1).sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-12-2018 a las 16:15:02
+-- Versión del servidor: 5.7.22-log
+-- Versión de PHP: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `newsattack`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rss`
+--
+
+CREATE TABLE `rss` (
+  `id_rss` int(11) NOT NULL,
+  `url_rss` varchar(256) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `rss`
+--
+
+INSERT INTO `rss` (`id_rss`, `url_rss`) VALUES
+(1, 'url del rss'),
+(2, 'otra url del rss'),
+(3, 'otra otra url del rss');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `suscripciones`
+--
+
+CREATE TABLE `suscripciones` (
+  `id_user` int(11) NOT NULL,
+  `id_rss` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `suscripciones`
+--
+
+INSERT INTO `suscripciones` (`id_user`, `id_rss`) VALUES
+(1, 1),
+(2, 2),
+(2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(32) COLLATE utf8_bin NOT NULL,
+  `pass` varchar(32) COLLATE utf8_bin NOT NULL,
+  `mail` varchar(32) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id_user`, `username`, `pass`, `mail`) VALUES
+(1, 'josefa', '1234', 'josefa@mail.com'),
+(2, 'venancio', '1234', 'venancio@mail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `rss`
+--
+ALTER TABLE `rss`
+  ADD PRIMARY KEY (`id_rss`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `rss`
+--
+ALTER TABLE `rss`
+  MODIFY `id_rss` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
